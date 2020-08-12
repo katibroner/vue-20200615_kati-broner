a37f2f8cf3ab18aca9e77e219a360168a2dc3156
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-08-12 10:41:40 +0300

    Added solution for task: CounterButton

--- 1-module/1-task/script.js
+++ 1-module/1-task/script.js
@@ -1,4 +1,15 @@
 import Vue from '/vendor/vue.esm.browser.js';
 
-// const app = ...
-// Рекомендуется использовать МЕТОД в качестве обработчика события
+export const app = new Vue({
+  el: '#app',
+
+  data: {
+    count: 0,
+  },
+
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+});
